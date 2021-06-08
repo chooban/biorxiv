5f324f322f9e57c848d57bddabdeb82b50167084
Ross Hendry <rhendry@googlemail.com>
2021-06-08 17:29:32 +0100

    Added a comment

--- biorxiv/biorxiv.py
+++ biorxiv/biorxiv.py
@@ -3,6 +3,11 @@ from requests_html import HTMLSession
 def search(s):
     session = HTMLSession()
     request = session.get(s)
+
+    # We need this module rather than just 'requests' as the
+    # part of the page which shows the publication status is
+    # rendered after the initial page load, using JavaScript.
+    # requests_html allows us to execute that JS if we call render().
     request.html.render()
 
     title = request.html.find('h1#page-title')[0].text
@@ -12,4 +17,4 @@ def search(s):
         return (title,"No publication found")
 
     return (s, title, element[0].text)
-    
+
